758f1e96a3e656a44ae40969913cb744957b4171
wests <wests@C-010053>
2022-02-15 14:33:33 +0000

    Added External Auth

--- PictureShare/Program.cs
+++ PictureShare/Program.cs
@@ -6,13 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication()
+    .AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"]; 
+        googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
+    });
 
 var app = builder.Build();
 
