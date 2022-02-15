c3102e9785af7c6b51ebb26126ea5c23fc6c620e
wests <wests@C-010053>
2022-02-15 10:27:36 +0000

    Added Bootstrap 5

--- PictureShare/Controllers/PictureController.cs
+++ PictureShare/Controllers/PictureController.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PictureShare.Data;
 
 namespace PictureShare.Controllers
 {
     public class PictureController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public PictureController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var pictures = await _db.Pictures.ToListAsync();
+            return View(pictures);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
